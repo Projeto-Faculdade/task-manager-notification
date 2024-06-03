e6dea280e27eb79e36dec966051a421c19a0c5af
jersonb <jersonlbrito@hotmail.com>
2024-06-02 22:08:03 -0300

    feat: new task

---
+++ src/TaskManagerNotification.WorkerService/Emails/NewTask/NewTaskEnglisEmail.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManagerNotification.WorkerService.Emails.NewTask;
+
+internal class NewTaskEnglisEmail : IEmail
+{
+    private string FirstName => Recipients[0].Name.Split(" ")[0];
+
+    public List<Recipient> Recipients { get; set; } = []!;
+
+    public string Key => "newtask-en";
+
+    public string Subject => "New activity for you";
+
+    public string PlainText => $"Hi {FirstName}! You have a new activity.";
+
+    public string Content => $"Hi {FirstName}! You have a new activity.";
+}
